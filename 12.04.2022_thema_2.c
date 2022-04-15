f9cd2700b325b4475cef3b53eee9e3348c34eca5
VK <93388918+vag4me@users.noreply.github.com>
2022-04-15 12:35:47 +0300

    Update 12.04.2022_thema_2.c

--- 12.04.2022_thema_2.c
+++ 12.04.2022_thema_2.c
@@ -34,3 +34,27 @@ int main() {
 		exit(1);
 	}
 	
+	walker = 500; // arxiki thesi
+	
+	for(i = 0; i < 1000; i++)
+	{
+		walker = walks(walker,10); // gia 10 ripseis 
+		printf("gia 10 ripseis = %d\n",walker);
+		fprintf(fp,"%d\n",walker);
+		walker = 500; // arxki thesi
+	
+		walker = walks(walker,50); // gia 50 ripseis
+		printf("gia 50 ripseis = %d\n",walker);
+		fprintf(fp,"%d\n",walker);
+		walker = 500; // arxiki thesi
+	
+		walker = walks(walker,100); // gia 100 ripseis
+		printf("gia 100 ripseis = %d\n",walker);
+		fprintf(fp,"%d\n",walker);
+		walker = 500; // arxiki thesi
+		printf("\n");
+	}
+	
+	fclose(fp);
+	return 0;
+}
